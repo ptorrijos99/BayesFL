3b2d90d889b0f62b8f12e83d58727a6f548240b8
Pablo Torrijos Arenas <pablo.torrijos@uclm.es>
2026-07-10 15:23:56 +0200

    test: add JUnit 5 test infrastructure

---
+++ src/test/java/bayesfl/SmokeTest.java
@@ -0,0 +1,11 @@
+package bayesfl;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SmokeTest {
+    @Test
+    void junitRuns() {
+        assertEquals(4, 2 + 2);
+    }
+}
